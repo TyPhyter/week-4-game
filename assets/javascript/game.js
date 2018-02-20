8ab2cb2cdad6b2ba5808da64b476d8c8022b4bc3
TyPhyter <tylerjenkins08@gmail.com>
2018-02-20 15:39:52 -0600

    character object created, some pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+
+
+var game = {
+    characters : {
+        //add path for images to these objects
+        "Obi-Wan" : {
+            name : "Obi-Wan",
+            hp : 100,
+            attack : 25,
+            counterAttack : 25,
+        },
+    
+        "Yoda" : {
+            name : "Yoda",
+            hp : 75,
+            attack : 20,
+            counterAttack : 40,
+        },
+
+        "Vader" : {
+            name: "Darth Vader",
+            hp : 125,
+            attack: 30,
+            counterAttack: 30,
+        },
+
+        "Mace" : {
+            name: "Mace Windu",
+            hp: 100,
+            attack: 25,
+            counterAttack: 25,
+        }
+    },
+
+    //use character array to generate divs with images and stats
+
+    //choose a character, assign playerCharacter
+
+    //maybe add not chosen characters to enemies array
+
+    //move divs to appropriate areas of the screen
+
+    //click enemy to target
+
+    //click attack (or something cooler) to initiate attack sequence
+
+    //decrement enemy hp by player attack value, vis versa, and increase attack
+        //if hp <=0 thing dies
+
+
+
+
+}
